b17a4a88282222262b97abe25ddd9a665245e6ee
Ciper100 <97075159+Ciper100@users.noreply.github.com>
2023-02-04 12:52:23 +0545

    a

--- Main Concepts/4.StateAndLifecycle.jsx
+++ Main Concepts/4.StateAndLifecycle.jsx
@@ -1,20 +1,23 @@
 class Clock extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { date: new Date() };
+        this.state = { date: new Date() }; //this.state is a object which is assigned a object literal with key value pair,this state gets updated later
     }
-    componentDidMount() {
+    componentDidMount() {// Inside this lifecycle method, the Clock component asks the browser to set up a timer to call the component’s tick() method once a second.
         this.timerID = setInterval(
             () => this.tick(), 1000
-        )
+        );
     }
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
-    tick() {
-        this.setState({
+
+    tick() {//Every second the browser calls the tick() method. Inside it, the Clock component schedules a UI update by calling setState() with an object containing the current time.
+
+        this.setState({//Thanks to the setState() call, React knows the state has changed, and calls the render() method again to learn what should be on the screen. This time, this.state.date in the render() method will be different, and so the render output will include the updated time. React updates the DOM accordingly.
             date: new Date()
         });
+    }
+
+    componentWillUnmount() {  //If the Clock component is ever removed from the DOM, React calls the componentWillUnmount() lifecycle method so the timer is stopped.
+        clearInterval(this.timerID);
     }
 
     render() {
@@ -29,4 +32,4 @@ class Clock extends React.Component {
 }
 
 const root = ReactDOM.CreateRoot(document.getElementById('root'));
-root.render(<Clock />);
+root.render(<Clock />);//this tells react what should be displayed on the screen
